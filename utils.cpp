1fe1df0da85d273b6fe6f8cdbe86ee4c9ae5f7f1
Al0den <111601320+Al0den@users.noreply.github.com>
2024-08-07 12:35:06 +0200

    Fix: bug regarding namespace

--- utils.cpp
+++ utils.cpp
@@ -1,19 +1,17 @@
 #include "./utils.hpp"
 
-using namespace neat;
-
-int randint(int min, int max) {
+int neat::randint(int min, int max) {
     static std::random_device rd;
     static std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(min, max);
     return dis(gen);
 }
 
-double max(double a, double b) {
+double neat::max(double a, double b) {
     if(a>b) return a;
     return b;
 }
-double min(double a, double b) {
+double neat::min(double a, double b) {
     if(a<b) return a;
     return b;
 }
